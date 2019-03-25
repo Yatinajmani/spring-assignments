e20349273c3ee6e8944ef437f80e379fc1d20b9a
Yatin Ajmani <yatinajmani@gmail.com>
2019-03-25 20:53:40 +0530

    Jpa Exercise 6,7,8,9 added,3 Updated.

--- Mar25_SpringDataJpa/spring-data-jpa/src/main/java/Main.java
+++ Mar25_SpringDataJpa/spring-data-jpa/src/main/java/Main.java
@@ -4,21 +4,81 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import repository.PersonRepository;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 /**
- * Perform all the methods inside CrudRepository for Person Class.
- * Use the methods declared above to fetch the person.
+ * 3.Perform all the methods inside CrudRepository for Person Class.
+ * 5.Use the methods declared above to fetch the person.
  */
 public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
         PersonRepository personRepository = (PersonRepository) context.getBean("personRepository");
-        personRepository.save(new Person("yatin", "ajmani", 54, 450000));
-        personRepository.save(new Person("yatin", "", 54, 450000));
+
+//      Exercise 3
+//      Using save(S entity)
+        personRepository.save(new Person("yatin", "ajmani", 24, 450000));
+
+//      Using save(Iterable<S> entities)
+        List<Person> people = Arrays.asList(new Person("yatin", "ajmani", 25, 430000),
+                new Person("harsh", "jain", 25, 420000),
+                new Person("fName", "lName", 24, 420000),
+                new Person("Peter", "parker", 26, 410000));
+        personRepository.save(people);
+
+//      Using findOne(ID id)
+        System.out.println(personRepository.findOne(1));
+
+//      Using exists(ID id)
+        System.out.println(personRepository.exists(1));
+
+//      Using findAll()
+        System.out.println("=====================Find All Records=====================");
         personRepository.findAll().forEach(System.out::println);
-        System.out.println(personRepository.findById(1));
-        System.out.println(personRepository.findByFirstName("yatin"));
-        System.out.println(personRepository.findByLastName("ajmani"));
+
+//      Using findAll(Iterable<ID> ids)
+        System.out.println("=====================Find All Records having ids 2,3,4.=====================");
+        personRepository.findAll(people.stream().map(Person::getId).collect(Collectors.toList()))
+                .forEach(System.out::println);
+//      Using count()
+        System.out.println("No. of Persons : " + personRepository.count());
+
+//      Using delete(ID id)
+        personRepository.delete(4);
+
+//      Using delete(T entity)
+        personRepository.delete(personRepository.findById(1));
+
+//      Using delete(Iterable<? extends T> entities)
+        personRepository.delete(Arrays.asList(people.get(0), people.get(1)));
+
+//      Using deleteAll()
+        personRepository.deleteAll();
+
+        personRepository.save(people);
+
+//      Exercise 5
+        System.out.println("Person with Id 6 : " + personRepository.findById(6));
+        System.out.println("Persons with FirstName yatin : " + personRepository.findByFirstName("yatin"));
+        System.out.println("Persons with lastName ajmani : " + personRepository.findByLastName("ajmani"));
+
+
+//      Exercise 6
+        System.out.println("Persons with age = 25 : " + personRepository.findByAgeViaQuery());
+
+//      Exercise 7
+        System.out.println("Persons full Name with age = 25 : ");
+        personRepository.findFullNameWithAge25ViaQuery().forEach(System.out::println);
+
+//      Exercise 8
+        System.out.println("Persons with age = 25 : ");
+        personRepository.findPersonWithAgeViaQuery().forEach(System.out::println);
+
+//      Exercise 9
+        System.out.println("Persons with first Name = Peter : " + personRepository.findPersonWithNamePeterViaQuery());
     }
 }
--- Mar25_SpringDataJpa/spring-data-jpa/src/main/java/repository/PersonRepository.java
+++ Mar25_SpringDataJpa/spring-data-jpa/src/main/java/repository/PersonRepository.java
@@ -1,19 +1,43 @@
 package repository;
 
 import entity.Person;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 
 /**
- * Implement CrudRepository for it.
- * For class Person find person declare methods in repository to find person by firstname, lastname and Id.
+ * 2.Implement CrudRepository for it.
+ * 4.For class Person find person declare methods in repository to find person by firstname, lastname and Id.
+ * 6.Use @Query to fetch firstname of the Person whose age is 25.
+ * 7.Use @Query to fetch firstname and lastname of the Person whose age is 25.
+ * 8.Get complete information of the Employee whose age is 25 using @Query.
+ * 9.Count Person where name is "Peter" using @Query.
  */
+//  Exercise 2
 public interface PersonRepository extends CrudRepository<Person, Integer> {
+    //  Exercise 4
     List<Person> findByFirstName(String firstName);
 
     List<Person> findByLastName(String lastName);
 
     Person findById(Integer id);
+
+    //  Exercise 6
+    @Query("select firstName from Person where age=25")
+    List<Person> findByAgeViaQuery();
+
+    //  Exercise 7
+    @Query("select CONCAT(p.firstName, ' ', p.lastName) as fullname from Person p where age=25")
+    List<String> findFullNameWithAge25ViaQuery();
+
+    //  Exercise 8
+    @Query("from Person where age=25")
+    List<Person> findPersonWithAgeViaQuery();
+
+    //  Exercise 9
+    @Query("select count(*) from Person where firstName='Peter'")
+    Integer findPersonWithNamePeterViaQuery();
+
 }
