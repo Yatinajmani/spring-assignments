933499ad6eb0bbdc6a2eb02c71cbc3f19afa93c4
Yatin Ajmani <yatinajmani@gmail.com>
2019-03-25 15:50:10 +0530

    Jpa Exercise 2,3 added.

--- Mar25_SpringDataJpa/spring-data-jpa/src/main/java/Main.java
+++ Mar25_SpringDataJpa/spring-data-jpa/src/main/java/Main.java
@@ -4,11 +4,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import repository.PersonRepository;
 
+
+/**
+ * Perform all the methods inside CrudRepository for Person Class.
+ */
 public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
         PersonRepository personRepository = (PersonRepository) context.getBean("personRepository");
         personRepository.save(new Person("yatin", "ajmani", 54, 450000));
+        personRepository.findAll().forEach(System.out::println);
     }
 }
--- Mar25_SpringDataJpa/spring-data-jpa/src/main/java/repository/PersonRepository.java
+++ Mar25_SpringDataJpa/spring-data-jpa/src/main/java/repository/PersonRepository.java
@@ -1,11 +1,13 @@
 package repository;
 
 import entity.Person;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
+/**
+ * Implement CrudRepository for it.
+ */
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 }
